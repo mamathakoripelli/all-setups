7a35ca65dd6fce108e644127c533bfa8fb9a0f4f
mamatha koripelli <Koripellimamatha@gmail.com>
2025-05-21 11:57:47 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,7 +2,7 @@ amazon-linux-extras install java-openjdk11 -y
 wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.7/bin/apache-tomcat-11.0.7.tar.gz
 tar -zxvf apache-tomcat-11.0.7.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.7/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.7/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.7/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.7/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.7/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-11.0.7/conf/tomcat-users.xml
